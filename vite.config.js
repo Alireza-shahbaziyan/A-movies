2463d775e17caa04df4f036364c554bd51ea5496
alireza-shahbaziyan <alishahbazi0831@gmail.com>
2022-08-18 01:18:15 +0430

    change vite.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/a-movies/',
+  base:'/A-movies/',
   plugins: [vue()],
   resolve: {
     alias: {
