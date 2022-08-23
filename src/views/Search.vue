65ea29854e07c41728122d63be84aa3b26e4f654
alireza-shahbaziyan <alishahbazi0831@gmail.com>
2022-08-23 16:23:13 +0430

    search screen

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-            <h1 class="text-2xl font-semibold	text-teal-400" >search</h1>
+    <div class="h-4/6 w-full flex flex-col justify-center items-center">
+            <div class="text-2xl font-semibold	text-teal-400" >search</div>
     </div>
 </template>
 <script>
